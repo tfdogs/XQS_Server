fff7a8635316b27d4890a6dcaea82ee71de55728
xasq <zqvvvv@126.com>
2017-08-01 20:01:25 +0800

    PHP

--- server/load_file.php
+++ server/load_file.php
@@ -2,7 +2,9 @@
 $httphead = explode(" ",$buf);
 $filename = substr($httphead[1],1);
 $files = explode("/",$filename);
+//echo substr($filename,0,stripos($filename,"?"));
 $base = "";
+$out = array();
 $msg = null;
 foreach ($files as $values){
     $base = $base.$values;
@@ -12,19 +14,18 @@ if(file_exists($info_detail["base"]."$filename") or substr($filename,-1) == "/")
         $i = 0;
         foreach ($index_files as $value){
             if(file_exists($info_detail["base"]."$base/$value")){
-                /*
-                if(strstr($value,".php")){
+                if(strstr($value,".php") and $info_detail["php_open"] == "open"){
                     $msg = null;
-                    exec($info_detail["php_root"]." ".$info_detail["base"].$filename,$out, $status);
+                    exec($info_detail["php_root"]."php-cgi.exe ".$info_detail["base"].$filename."index.php",$out, $status);
                     foreach ($out as $v){
                         $msg = $msg.$v;
                     }
-                }else{*/
+                }else{
                     $msg = "<!DOCTYPE html>".file_get_contents($info_detail["base"]."$base/$value");
-                    //}
+                    }
                 $i = $i + 1;
                 }
-            echo date("Y-m-d l H:i:s")." [200] Get success.$value \n";
+            echo date("Y-m-d l H:i:s")." [200] $value \n";
             if($i != 0){
                 break;
             }
@@ -32,24 +33,24 @@ if(file_exists($info_detail["base"]."$filename") or substr($filename,-1) == "/")
         if($i == 0){
             $msg = "<!DOCTYPE html><h2>Http status 403 - Forbidden.</h2><hr><p>Server rejected your requst for '$filename'.
                    <br>XQS Server 0.1.0 run by PHP ".PHP_VERSION;
-            echo date("Y-m-d l H:i:s")." [403] Forbidden.$filename \n";
+            echo date("Y-m-d l H:i:s")." [403] $filename \n";
         }
     }else{
-        /*
-        if(strstr($filename,".php")){
+
+        if(strstr($filename,".php")  and $info_detail["php_open"] == "open"){
             $msg = "1";
-            exec($info_detail["php_root"]." ".$info_detail["base"].$filename,$out, $status);
+            exec($info_detail["php_root"]."php-cgi.exe ".$info_detail["base"].$filename,$out, $status);
             foreach ($out as $value){
                 $msg = $msg.$value;
             }
-        }else{*/
+        }else{
             $msg = file_get_contents($info_detail["base"]."$filename");
-       // }
-        echo date("Y-m-d l H:i:s")." [200] Get success.$filename \n";
+       }
+        echo date("Y-m-d l H:i:s")." [200] $filename \n";
     }
 
 }else{
-    $msg = "<!DOCTYPE html><h2>Http status 404 - Not found.</h2><hr><p>File '$filename'could not found in the server.
+    $msg = "<!DOCTYPE html><h2>Http status 404 - Not found.</h2><hr><p>'$filename'could not found in the server.
             <br>XQS Server 0.1.0 run by PHP ".PHP_VERSION;
-    echo date("Y-m-d l H:i:s")." [404] Not found.$filename \n";
+    echo date("Y-m-d l H:i:s")." [404] $filename \n";
 }
